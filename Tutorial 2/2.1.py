77843feb7e90930e0063f66ca59b6ad551434135
shabarishoganb <131852273+shabarishoganb@users.noreply.github.com>
2025-03-14 02:07:59 +0530

    Delete Tutorial 2 directory

--- Tutorial 2/2.1.py
+++
@@ -1,5 +0,0 @@
-def remove_vowels(s):
-    return "".join(char for char in s if char.lower() not in "aeiou")
-
-input_string = "Hello, World!"
-print("String after removing vowels:", remove_vowels(input_string))
--- Tutorial 2/2.7.py
+++
@@ -1,6 +0,0 @@
-#Write a Program to converting all lowercase letters into uppercase
-
-s= input(" Enter the string : ")
-s=s.upper()
-
-print(s)
